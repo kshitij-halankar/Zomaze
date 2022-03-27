eef20bd81fee892e75d857c5663c6887b0c67609
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2022-03-27 01:56:06 -0400

    Added signup using Azure Playfab

---
+++ Zomaze/Assets/Scripts/SignUpScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class SignUpScript : MonoBehaviour
+{
+    public TMP_InputField userName;
+    public TMP_InputField userEmail;
+    public TMP_InputField userPassword;
+
+    //public GameObject userName, userEmail, userPassword;
+    string encryptedPassword;
+
+   public void SignUp(){
+        var registerRequest = new RegisterPlayFabUserRequest
+        {
+            Username = userName.text,
+            Email = userEmail.text,
+            Password = Encrypt(userPassword.text)
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterRequestSuccess, OnRegisterFailure);
+
+        //Debug.Log(
+        //   userName.text + " "
+        //    + userEmail.text + " "
+        //    + userPassword.text
+        // );
+
+    }
+
+    public void OnRegisterRequestSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("User Registered");
+    }
+
+    public void OnRegisterFailure(PlayFabError error)
+    {
+        Debug.Log("Registeration Failed");
+        Debug.Log(error.ErrorMessage);
+    }
+
+    string Encrypt(string pw)
+    {
+        System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] epw = System.Text.Encoding.UTF8.GetBytes(pw);
+        epw = x.ComputeHash(epw);
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+        foreach (byte b in epw)
+        {
+            s.Append(b.ToString("x2").ToLower());
+        }
+        return s.ToString();
+    }
+}
